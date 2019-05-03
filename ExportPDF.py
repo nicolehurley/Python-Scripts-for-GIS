30626ca14b3f4088bb4ce1ebf8cf8ff8c70f153d
Nicki Hurley <nicolehurley57@gmail.com>
2019-05-03 14:06:47 -0700

    Update ExportPDF.py

--- ExportPDF.py
+++ ExportPDF.py
@@ -1,16 +1,11 @@
 # N. Hurley, 1/2/2018
-# This script runs through all the mxds in a folder and imports the shapefiles
-# in those mxds into a new file geodatabase. The script skips data already
-# located in gdb. The script uses a project area shapefile to identify the
-# appropriate spatial reference information and can clip all data imported to
-# a buffered extent of that project area. After running this script, use
-# ReplaceWorkspaces.py to re-source all your MXDs to the new geodatabase.
+# This script runs through all the mxds in a folder and exports them to a pdf.
 
 import arcpy, os, sys
 
 # Set workspace. Filepath for the folder of existing mxds from which to pull
 # data.  Be sure to leave the 'r', i.e. r"C:\MyFilePath".
-arcpy.env.workspace = r"M:\Portland\USFS_Phosphate_Mines\Maps_All\MXDs"
+arcpy.env.workspace = r"M:\file_location"
 Workspace = arcpy.env.workspace 
 
 
